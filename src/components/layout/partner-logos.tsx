49e4077ae7c42f703c410ebad842367098e6377b
KAUSHIK SARKAR <drksarkar86@gmail.com>
2025-09-23 03:35:23 +0000

    The logos are appearing as broken images. Please correct that error

--- src/components/layout/partner-logos.tsx
+++ src/components/layout/partner-logos.tsx
@@ -5,6 +5,7 @@ const logos = [
   { src: '/logos/mis.png', alt: 'MIS Logo' },
   { src: '/logos/iedcr.png', alt: 'IEDCR Logo' },
   { src: '/logos/unops.png', alt: 'UNOPS Logo' },
+  { src: '/logos/brac.png', alt: 'BRAC Logo' },
   { src: '/logos/moru.png', alt: 'MORU Logo' },
   { src: '/logos/gm.png', alt: 'GM Logo' },
   { src: '/logos/imacs.png', alt: 'IMACS Logo' },
